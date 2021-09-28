618896358a5238237fb02171868c114934db2bf1
Artha <83387287+Ekalavyaaa@users.noreply.github.com>
2021-09-28 13:09:28 +0800

    mengubah file ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,16 +21,17 @@ class ProductController extends Controller
             $query = Product::query();
 
             return DataTables::of($query)
-                ->addColumn('action', function($item){
+                ->addColumn('action', function ($item) {
                     return '
-                        <a href="'. route('dashboard.product.edit', $item->id) .'" class="bg-gray-500 text-white rounded-md px-2 py-1 m-2">
+                        <a href="' . route('dashboard.product.edit', $item->id) . '" class="bg-gray-500 text-white rounded-md px-2 py-1 m-2">
                             Edit
                         </a>
-                        <form class="inline-block" action="'. route('dashboard.product.destroy', $item->id) .'" method="POST">
+
+                        <form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
                             <button class="bg-gray-500 text-white rounded-md px-2 py-1 m-2">
                                 Hapus
                             </button>
-                            '. method_field('delete') .csrf_field()  .'
+                            ' . method_field('delete') . csrf_field()  . '
                         </form>
                     ';
                 })
@@ -61,13 +62,12 @@ class ProductController extends Controller
      */
     public function store(ProductRequest $request)
     {
-         $data = $request->all();
-         $data['slug'] = Str::slug($request->name);
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
 
-         Product::create($data);
+        Product::create($data);
 
-         return redirect()->route('dashboard.product.index');
-
+        return redirect()->route('dashboard.product.index');
     }
 
     /**
@@ -90,7 +90,7 @@ class ProductController extends Controller
     public function edit(Product $product)
     {
         return view('pages.dashboard.product.edit', [
-                'item' => $product
+            'item' => $product
         ]);
     }
 
@@ -109,7 +109,6 @@ class ProductController extends Controller
         $product->update($data);
 
         return redirect()->route('dashboard.product.index');
-
     }
 
     /**
