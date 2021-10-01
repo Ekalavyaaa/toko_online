f6c217e784731796f0365ab055cd433c3bb747e1
Artha <83387287+Ekalavyaaa@users.noreply.github.com>
2021-10-01 13:04:19 +0800

    mengubah file show.blade.php

--- resources/views/pages/dashboard/transaction/show.blade.php
+++ resources/views/pages/dashboard/transaction/show.blade.php
@@ -24,18 +24,58 @@
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <h2 class="font-semibold text-lg text-gray-800 leading-tight mb-5">
+              Transaction Details
+          </h2>
+
+          <div class="bg-white overflow-hidden shadow sm:rounded-lg mb-10">
+              <div class="p-6 bg-white border-b border-gray-200">
+                  <table class="table-auto w-full">
+                      <tbody>
+                          <tr>
+                              <th class="border px-6 py-4 text-right">Nama</th>
+                              <td class="border px-6 py-4">{{ $transaction->name }}</td>
+
+                              <th class="border px-6 py-4 text-right">Email</th>
+                              <td class="border px-6 py-4">{{ $transaction->email }}</td>
+
+                              <th class="border px-6 py-4 text-right">Address</th>
+                              <td class="border px-6 py-4">{{ $transaction->address }}</td>
+
+                              <th class="border px-6 py-4 text-right">Phone</th>
+                              <td class="border px-6 py-4">{{ $transaction->phone }}</td>
+
+                              <th class="border px-6 py-4 text-right">Courier</th>
+                              <td class="border px-6 py-4">{{ $transaction->courier }}</td>
+
+                              <th class="border px-6 py-4 text-right">Payment</th>
+                              <td class="border px-6 py-4">{{ $transaction->payment }}</td>
+
+                              <th class="border px-6 py-4 text-right">Payment URL</th>
+                              <td class="border px-6 py-4">{{ $transaction->payment_url }}</td>
+
+                              <th class="border px-6 py-4 text-right">Total Price</th>
+                              <td class="border px-6 py-4">{{ number_format($transaction->total_price) }}</td>
+
+                              <th class="border px-6 py-4 text-right">Status</th>
+                              <td class="border px-6 py-4">{{ $transaction->status }}</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+
+          <h2 class="font-semibold text-lg text-gray-800 leading-tight mb-5">
+              Transaction Item
+          </h2>
         <div class="shadow overflow-hidden sm-rounded-md">
             <div class="px-4 py-5 bg-white sm:p-6">
                 <table id="crudTable">
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Nama</th>
-                            <th>Telepon</th>
-                            <th>Kurir</th>
-                            <th>Total harga</th>
-                            <th>Status</th>
-                            <th>Aksi</th>
+                            <th>Produk</th>
+                            <th>Harga</th>
                         </tr>
                     </thead>
                     <tbody></tbody>
