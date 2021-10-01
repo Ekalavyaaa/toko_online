5f74bcc14122c2b0f9f460c172eb10d9efa7b820
Artha <83387287+Ekalavyaaa@users.noreply.github.com>
2021-10-01 13:02:53 +0800

    menambahkan TransactionController

---
+++ resources/views/pages/dashboard/transaction/show.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          Transaction &raquo; #{{ $transaction->id }} {{ $transaction->name }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="script">
+        <script>
+            // AJAX Datatable
+
+            var datatable = $('#crudTable').DataTable({
+                ajax: {
+                    url: '{!! url()->current()  !!}'
+                },
+                columns: [
+                    {data: 'id', name: 'id',width: '5%'},
+                    {data: 'product.name', name: 'product.name'},
+                    {data: 'product.price', name: 'product.price'}
+                ]
+            })
+        </script>
+    </x-slot>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden sm-rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+                <table id="crudTable">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Telepon</th>
+                            <th>Kurir</th>
+                            <th>Total harga</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
